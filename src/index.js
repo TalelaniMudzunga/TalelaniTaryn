d7bff1449cd9906505e53c606b0612e380ecfade
Talelani Taryn Mudzunga <mudzungatalelani@outlook.com>
2025-07-31 18:55:22 +0200

    added api in the js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+function searchCity() {
+    let apiKey = "4798o43t6a95dd37517f901a314b0ebe";
+    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key={lat}&key=${apiKey}&units=metric';
+}
 function handleSearch(event) {
     event.preventDefault(); 
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value.trim();
+    searchCity(searchInput.value); 
 }
 
 let searchFormElement = document.querySelector("#search-form");
