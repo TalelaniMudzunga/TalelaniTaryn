c6b450f8b168a619dd8926e3250d7f6cb807e3a7
Talelani Taryn Mudzunga <mudzungatalelani@outlook.com>
2025-07-31 19:35:18 +0200

    create search function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-function searchCity() {
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+
+    temperatureElement.innerHTML = Math.round(temperature);
+       
+}
+function searchCity(city) {
     let apiKey = "4798o43t6a95dd37517f901a314b0ebe";
-    let apiUrl = 'https://api.shecodes.io/weather/v1/current?query=${city}&key={lat}&key=${apiKey}&units=metric';
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
 }
 function handleSearch(event) {
     event.preventDefault(); 
     let searchInput = document.querySelector("#search-form-input");
+    let city = searchInput.value.trim();
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value.trim();
-    searchCity(searchInput.value); 
+    cityElement.innerHTML = city;
+    searchCity(city); 
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+searchCity("Soweto"); 
     
