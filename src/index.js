cc61d34144755a21cdc029b45e821b86e8ab0647
Talelani Taryn Mudzunga <mudzungatalelani@outlook.com>
2025-08-02 19:11:13 +0200

    to change the weather condition
    injected the js for the image icon

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ function refreshWeather(response) {
     let windSpeedElement = document.querySelector("#wind-speed");
     let timeElement = document.querySelector("#time");
     let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#weather-icon"); 
     
 
     cityElement.innerHTML = response.data.city;
@@ -19,7 +20,9 @@ function refreshWeather(response) {
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
-       
+      
+    iconElement.src = response.data.condition.icon_url;
+    iconElement.alt = response.data.condition.description;
 }
 function searchCity(city) {
     let apiKey = "4798o43t6a95dd37517f901a314b0ebe";
